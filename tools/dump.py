ddaf2063a30c7f485d05acabe0db75e09569d0b3
Krist10an <krist10an@hotmail.com>
2015-01-07 21:05:47 +0100

    Add registration, type and country to dump

--- tools/dump.py
+++ tools/dump.py
@@ -3,9 +3,9 @@ import sqlite3 as lite
 def main(dbfile):
     thelist = []
     con = lite.connect(dbfile)
-    with con:    
+    with con:
         cur = con.cursor()
-        cur.execute("SELECT * FROM trackslog WHERE lat>0")
+        cur.execute("SELECT t.*, a.registration, a.ICAOTypeCode, a.modescountry FROM trackslog t JOIN Aircraft a ON (t.modes=a.modes) WHERE lat>0")
         for row in cur.fetchall():
 		print ";".join([str(x) for x in row])
 
