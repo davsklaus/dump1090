635e4c772d577364aaeb5863fef08144e7db5d81
klaus <davsklaus@gmail.com>
2016-07-17 14:47:03 +0000

    fix data logging

--- tools/create_tables.sql
+++ tools/create_tables.sql
@@ -26,8 +26,28 @@ CREATE TABLE trackslog (
     lon         DECIMAL( 17, 14 ),
     heading     INT( 6 ),
     speed       INT( 6 ),
+    flight      VARCHAR( 7 ),
     last_update TIMESTAMP          DEFAULT ( CURRENT_TIMESTAMP ) 
 );
+
+CREATE TABLE flights (
+       df          SMALLINT( 2 ),
+       modes       CHAR( 6 )          UNIQUE ON CONFLICT REPLACE,
+       country     CHAR( 2 ),
+       flight      VARCHAR( 7 ),
+       airline     CHAR( 3 ),
+       squawk      INT( 4 ),
+       regn        VARCHAR( 16 ),
+       type        VARCHAR( 16 ),
+       msgs        INT( 6 ),
+       last_update TIMESTAMP          DEFAULT ( CURRENT_TIMESTAMP ) ,
+       alt         INT( 6 ),
+       vr          SMALLINT( 2 ),
+       lat         DECIMAL( 17, 14 ),
+       lon         DECIMAL( 17, 14 ),
+       heading     INT( 6 ),
+       speed       INT( 6 )
+ );
 
 CREATE VIEW planes AS
        SELECT f.modes,
